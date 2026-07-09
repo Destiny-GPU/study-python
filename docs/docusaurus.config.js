88e02738a06877b34309a04de4c46a95763ae41b
Destiny-GPU <189123327+Destiny-GPU@users.noreply.github.com>
2026-07-10 02:50:53 +0800

    修复 baseUrl 适配 Vercel 部署

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://Destiny-GPU.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/study-python/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
