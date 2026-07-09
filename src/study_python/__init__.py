44942d06afc530cd85494a4547aafa4235491b8a
Destiny-GPU <189123327+Destiny-GPU@users.noreply.github.com>
2026-07-10 01:22:15 +0800

    second commit

---
+++ src/study_python/__init__.py
@@ -0,0 +1,3 @@
+"""My Project - 我的第一个现代 Python 项目。"""
+
+__version__ = "0.1.0"
---
+++ src/study_python/main.py
@@ -0,0 +1,10 @@
+from study_python.models import a
+
+
+def main():
+    print("Hello from study-python!")
+
+
+if __name__ == "__main__":
+    main()
+    print(a)
